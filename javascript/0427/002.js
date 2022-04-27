74ba61c9aa2b9d632141bffae162472bea90adfc
YOOJS1205 <junsang8921@gmail.com>
2022-04-27 18:18:31 +0900

    0427

---
+++ javascript/0427/002.js
@@ -0,0 +1,5 @@
+document.getElementById('one').innerHTML = 
+'hello world <strong>document</strong>';
+console.log('hello world console');
+window.alert('hello world alert');
+document.write('hello!');
